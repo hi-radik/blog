e4a9cfa4b4bb7fd575b6754bbc4a841b32f84184
R <practice.radomir@gmail.com>
2022-07-18 13:58:24 +0300

    Some fixes added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -55,7 +55,7 @@ export const login = async (req,res) => {
             })
         }
 
-        const isValidPassword = bcrypt.compare(req.body.password, user._doc.passwordHash)
+        const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash)
         if (!isValidPassword) {
             return res.status(400).json({
                 message:'Ошибка авторизации'
